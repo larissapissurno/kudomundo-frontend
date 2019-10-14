6d7ea03931384ff53b60b43a910f79668311bbbb
Ramon Nunes Teixeira <ramon.teixeira@db1.com.br>
2019-10-14 17:30:10 -0300

    #7 criado estrutura para teste unitario com jest

---
+++ tests/unit/components/card.spec.js
@@ -0,0 +1,17 @@
+import { shallowMount } from '@vue/test-utils'
+import HelloWorld from '@/components/Card.vue'
+
+describe('Card.vue', () => {
+  it('renders card object when passed', () => {
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { 
+        card: {
+          title: 'teste',
+          member: 'fulano',
+          description: 'top demais'
+        }
+      }
+    })
+    expect(wrapper.text()).toBeTruthy()
+  })
+})
