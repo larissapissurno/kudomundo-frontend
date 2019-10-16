13bf699b5f558db131e01665d8d010ea09945fc2
Ramon Nunes Teixeira <ramon.teixeira@db1.com.br>
2019-10-16 14:22:23 -0300

    ajustado configuracao do babel

--- src/main.js
+++ src/main.js
@@ -5,19 +5,16 @@ import { default as Adal, AuthenticationContext } from 'vue-adal'
 import App from './App.vue'
 import router from './router'
 import VueToastr from 'vue-toastr'
-
 import Loading from 'vue-loading-overlay'
 // eslint-disable-next-line
 import 'vue-loading-overlay/dist/vue-loading.css'
-
 import { importCustomCss } from './tenant'
-
-Vue.use(Loading)
-Vue.use(VueToastr, {})
 
 Vue.config.productionTip = false
 const clientId = '026d3962-dfa5-4720-a65f-2c62d1edd4a8'
 
+Vue.use(Loading)
+Vue.use(VueToastr, {})
 Vue.use(Adal, {
   config: {
     tenant: 'ea47001a-3428-40f3-8ea1-86bdb1a3bc84',
